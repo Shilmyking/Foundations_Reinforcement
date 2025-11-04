44f63c4f430d3f3a97abe0879d2a09ed9dcd1367
aliao <aliao2414937928@qq.com>
2025-11-04 13:22:39 +0800

    initial commit

---
+++ LinkListed/21_mergeTwoLists.cpp
@@ -0,0 +1,71 @@
+﻿
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+ 
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)return list2;
+        if (list2 == nullptr) return list1;
+
+        //ListNode* head = list1->val < list2->val ? list1 : list2;
+        ListNode* head;
+        if (list1->val > list2->val) {
+            head = list2;
+            list2 = list2->next;
+        }
+        else {
+            head = list1;
+            list1 = list1->next;
+        }
+        ListNode* cur = head;//留下head
+        ListNode* nexts = cur;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            
+            if (list1->val > list2->val) {
+                nexts->next = list2;
+                list2 = list2->next;
+            }
+            else {
+                nexts->next = list1;
+                list1 = list1->next;
+            }
+            cur = nexts->next;
+            nexts = cur;
+        }
+        if (list1 != nullptr) {
+            nexts->next = list1;
+        }
+        if (list2 != nullptr) {
+            nexts->next = list2;
+        }
+        return head;
+    }
+
+    //递归：给到两个有序链表  返回将两个链表合并后的结果
+    //
+    ListNode* mergeTwoLists1(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)return list2;
+        if (list2 == nullptr) return list1;
+        //从小到大 依次串好
+        if (list1->val < list2->val) {
+            //递归
+            list1->next = mergeTwoLists(list1->next, list2);
+
+            //回溯到当前调用递归的位置   返回ans
+            return list1;
+        }
+
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+};
---
+++ LinkListed/23_mergeKLists.cpp
@@ -0,0 +1,183 @@
+﻿#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    //bool operator<(const ListNode* node)const {
+    //    return this->val < node->val;
+    //}//本地可以这样写  然后比较ListNode*  不对不对是比较了地址值的大小
+};
+
+class Solution {
+
+    //暴力
+    ListNode* f1(const vector<ListNode*>&lists) {
+        int n = lists.size();
+        if (n == 0)return nullptr;
+        ListNode* cur = lists[0];
+        //ListNode* head = cur;
+        for (int i = 1; i < n; i++) {
+            //返回的新head给到cur  因为存在换head的可能性
+            cur = mergeTwoLists(cur, lists[i]);
+        }
+        return cur;
+    }
+
+    ListNode* f2(const vector<ListNode*>& lists, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = (left + right) / 2;
+        ListNode* head;
+
+        ListNode* leftNode = f2(lists, left, mid);
+        ListNode* rightNode = f2(lists, mid + 1, right);
+
+        head = mergeTwoLists(leftNode, rightNode);
+        return head;
+        //return mergeTwoLists(f2(lists, left, mid), f2(lists, mid + 1, right));
+    }
+
+    ListNode* f3(const vector<ListNode*>& lists) {
+        if (lists.size() == 0) { return nullptr; }
+        build();
+        int n = lists.size();
+        for (ListNode*v:lists) {
+            if (v != nullptr){
+                push(v);
+            }
+        }       
+        if (isEmpty()) {//存在 lists就是 []的情况
+            return nullptr;
+        }
+        ListNode* head = pop();//拿堆顶
+        //只要pop了 就需要 把堆顶的下一个元素放进小根堆
+        ListNode* pre = head;
+        if (pre!=nullptr&&pre->next != nullptr) {
+            push(pre->next);
+        }
+        ListNode* cur;
+        while (!isEmpty()) {
+             cur = pop();
+             pre->next = cur;
+             pre = pre->next;//去下一个位置继续串
+             if (cur->next != nullptr) {
+                 push(cur->next);
+             }
+        }
+        return head;
+    }
+private:
+    static const int MAXN = 1e4 + 5;
+    ListNode* heap[MAXN];
+    int heapSize = 0 ;
+
+    //需要维持一个小根堆
+
+    void build() {
+        heapSize = 0;
+    }
+    //上浮操作
+    void heapInsert(int i) {
+        //如果当前节点
+        while (heap[i]->val < heap[(i - 1) / 2]->val) {
+            std::swap(heap[i], heap[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
+
+    //下沉操作  小根堆
+    void heapify(int i) {
+        int left = 2 * i + 1;
+        while (left < heapSize) {
+            //找到孩子节点较小的 
+            int smallLeaf = left + 1 < heapSize && heap[left + 1]->val < heap[left]->val ? left + 1 : left;
+            smallLeaf = heap[smallLeaf]->val < heap[i]->val ? smallLeaf : i;
+            if (smallLeaf == i) {
+                break;
+            }
+            std::swap(heap[smallLeaf], heap[i]);
+            i = smallLeaf;
+            left = 2 * i + 1;
+        }
+    }
+
+    void push(ListNode* node) {
+        //heapSize 指向要加入的位置
+        heap[heapSize] = node;
+        heapInsert(heapSize++);//维持小根堆
+    }
+
+    ListNode* pop() {
+        if (heapSize == 0) {
+            return nullptr;
+        }
+        ListNode* ans = heap[0];
+        //把堆底换上来
+        std::swap(heap[0], heap[--heapSize]);
+        heapify(0);
+        return ans;
+    }
+    bool isEmpty() {
+        return heapSize == 0;
+    }
+private:
+    // k最大为1e4(1W) 
+    // 每个链表长度不超过500
+    //所有链表节点总和不超过 1e4
+
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)return list2;
+        if (list2 == nullptr) return list1;
+
+        //ListNode* head = list1->val < list2->val ? list1 : list2;
+        ListNode* head;
+        if (list1->val > list2->val) {
+            head = list2;
+            list2 = list2->next;
+        }
+        else {
+            head = list1;
+            list1 = list1->next;
+        }
+        ListNode* cur = head;//留下head
+        ListNode* nexts = cur;
+
+        while (list1 != nullptr && list2 != nullptr) {
+
+            if (list1->val > list2->val) {
+                nexts->next = list2;
+                list2 = list2->next;
+            }
+            else {
+                nexts->next = list1;
+                list1 = list1->next;
+            }
+            cur = nexts->next;
+            nexts = cur;
+        }
+        if (list1 != nullptr) {
+            nexts->next = list1;
+        }
+        if (list2 != nullptr) {
+            nexts->next = list2;
+        }
+        return head;
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        //return f1(lists);
+        //return f2(lists, 0, lists.size() - 1);
+        return f3(lists);
+    }
+};
