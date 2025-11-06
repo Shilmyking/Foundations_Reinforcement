45c8d68e91ae9d22290be437ace51cca29869b6c
aliao <aliao2414937928@qq.com>
2025-11-06 20:47:18 +0800

    modify code

---
+++ LinkedList/138_copyRandomList.cpp
@@ -0,0 +1,57 @@
+﻿
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
+
+
+class Solution {
+public:
+    //三步走：
+    //  先生成复制节点 
+    //  然后设置好random指针  
+    // random指针设置为 cur->random->next(cur->random指向的就是要复制的节点的上一个)
+    // 原链表和新生成的节点分离 
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) { return head; }
+        Node* cur = head;
+        Node* next = cur->next;
+        //创建新节点
+        while (cur != nullptr) {
+            Node* node = new Node(cur->val);
+            cur->next = node;
+            node->next = next;
+            cur = next;
+
+            next = cur != nullptr ? cur->next : nullptr;
+        }
+        //设置random节点
+        cur = head;
+        while (cur != nullptr) {
+            next = cur->next;
+            next->random = cur->random != nullptr ? cur->random->next : nullptr;
+            cur = next;
+        }
+        //分离 cur 和 nexts
+        cur = head;
+        Node* copyHead = cur->next;
+        while (cur != nullptr) {
+            next = cur->next;
+            cur->next = next->next;
+            
+            next->next = cur->next;
+        }
+        return copyHead;
+    }
+};
---
+++ LinkedList/25_reverseKGroup.cpp
@@ -0,0 +1,61 @@
+﻿
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+    //翻转链表
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) { return head; }
+        ListNode* cur = head;
+        ListNode* next = head->next;
+        ListNode* pre = nullptr;
+        while (next != nullptr) {
+            cur->next = pre;//转头
+
+            pre = cur;//pre紧跟当前node ，因为当前node将变成下一个pre
+
+            cur = next;//到下一个
+
+            next = next->next;
+        }
+        cur->next = pre;
+        return cur;
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr) { return head; }
+        ListNode* dummy = new ListNode(-1, head);
+
+        ListNode* startPreNode = dummy;
+        ListNode* startNode = head;
+
+        while (startNode != nullptr) {
+            ListNode* endNode = startPreNode;
+            //check 是否还需要翻转
+            for (int i = 0; i < k; i++) {
+                endNode = endNode->next;
+                if (endNode == nullptr) {
+                    return dummy->next;
+                }
+            }//出循环说明就是需要翻转
+            ListNode* endNextNode = endNode->next;
+            endNode->next = nullptr;//需要断链
+
+            reverseList(startNode);
+
+            //翻转之后 endNode变为 NewSubNode  startNode变为末尾的Node
+            startPreNode->next = endNode;//前面部分
+            startNode->next = endNextNode;//翻转之后的末尾节点接上 上一次反转之前节点的
+
+            startPreNode = startNode;//下一次翻转的节点的前一个节点
+            startNode = endNextNode;//下一次翻转的新head
+        }
+        return dummy->next;
+    }
+};
