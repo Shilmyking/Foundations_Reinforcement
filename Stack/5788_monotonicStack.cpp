7b324df598be2286f78f9d42998247f9eefe9dcd
aliao <aliao2414937928@qq.com>
2025-11-11 17:00:39 +0800

    modify code

--- Stack/5788_monotonicStack.cpp
+++ Stack/5788_monotonicStack.cpp
@@ -1,13 +1,16 @@
 ﻿
+// 给定一个长度为n的数组，打印每个位置的右侧，大于该位置数字的最近位置
+// 测试链接 : https://www.luogu.com.cn/problem/P5788
+
 #include<iostream>
 #include<string>
 
 using namespace std;
-
+#if 0
 #define int long long
 #define endl "\n"
 
-const int N = 1e6 + 5;
+const int N = 3e6 + 5;
 int nums[N];
 int Stack[N];
 int Size;
@@ -16,20 +19,43 @@ int ans[N];
 int n;
 
 //这里题目 是洛谷的  要求右侧 第一个比自己大的元素的下标  不存在的话拿0替代
+//注意下标 从1开始 所以0位置弃而不用
 void compute() {
+	//遍历一次  需要找比当前位置大的 右侧的第一个数字的下标
+	//重复的数字设置为可以重复进入
+	for (int i = 1; i <= n; i++) {
+		while (Size != 0 && nums[i] > nums[Stack[Size - 1]]) {
+			//如果栈内存在元素  而且栈顶元素更小   可以结算栈顶元素了
+			int top = Stack[--Size];
+			//只看右侧
+			ans[top] = i;
+		}
+		Stack[Size++] = i;
+	}
 
+	//清算
+	//其实这一步都不用了   设置0在最初始的过程就做了
+	//while (Size > 0) {
+
+	//}
+
+	//重复的 数字也无需处理  因为在循环内的流程是 相同数字可以重复进入  
+	//遇到大的就重复结算
 }
+
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> nums[i];
 	}
 	compute();
-	cout << ans[0];
-	for (int i = 1; i < n; i++) {
+	cout << ans[1];
+	for (int i = 2; i <= n; i++) {
 		cout << " " << ans[i];
 	}
 	cout << endl;
 }
+
+#endif
---
+++ Stack/84_largestRectangleArea.cpp
@@ -0,0 +1,54 @@
+﻿
+
+// 柱状图中最大的矩形
+// 给定 n 个非负整数，用来表示柱状图中各个柱子的高度
+// 每个柱子彼此相邻，且宽度为 1 。求在该柱状图中，能够勾勒出来的矩形的最大面积
+// 测试链接：https://leetcode.cn/problems/largest-rectangle-in-histogram
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+class Solution {
+    static const int N = 1e5 + 5;
+    int Stack[N];
+    int Size = 0;
+    int Max = 0;
+public:
+    // 需要枚举每个位置的情况
+    //每个位置找到比当前位置 小的左侧位置  和小的右侧位置
+    // i-left-1  *  height[i]就是矩形高度
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        for (int i = 0; i < n; i++) {
+            // 大于等于的都 进行结算  这也相等的数字岂不是找不到正确的右边界
+            // 没关系 因为当前进入的这个相等的数字其实会接管这些pop出去的数字的左边界
+            // 右边界也会正确的找到 所以无伤大雅  最后还是能找到最大的矩阵
+
+            // 栈顶元素 大于等于 新进入的元素 这个时候结算  pop栈顶    保证的是大压小
+            // 这样就能保证  如果当前栈顶 和 下面的数字 存在i上的间隔没关系  因为间隔内的数字一定更大才结算了
+            // 也就是范围为  i - left -1
+            while (Size > 0 && heights[Stack[Size - 1]] >= heights[i]) {
+                int top = Stack[--Size];
+                // left 是 top 柱子左边第一个更矮的柱子索引
+                int left = Size > 0 ? Stack[Size - 1] : -1;
+                // 右边界是 i (当前导致 top 弹出的索引)
+                // 左边界是 left
+                //注意宽度 是 (i-left-1)  因为当前位置结算 
+                // 可能top位置 距离 left 和 i都有大段距离  但是都是有heights[i]高
+                Max = std::max(Max, (heights[top] * (i - left - 1)));
+            }
+            Stack[Size++] = i;
+        }
+
+        //清算剩余未计算的
+        while (Size > 0) {
+            int top = Stack[--Size];
+            int left = Size > 0 ? Stack[Size - 1] : -1;
+            // 这里就是 (n-left-1)
+            Max = std::max(Max, heights[top] * (n - left - 1));
+        }
+        return Max;
+    }
+};
