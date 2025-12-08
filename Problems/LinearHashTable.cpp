d3494b66c707ad0364072d669eb0cf561ddcd27e
aliao <aliao2414937928@qq.com>
2025-12-09 01:40:26 +0800

    modify code

--- Problems/LinearHashTable.cpp
+++ Problems/LinearHashTable.cpp
@@ -1,4 +1,19 @@
-﻿
+﻿/*
+增加： 通过哈希函数 计算存放位置
+该位置空闲 直接存储
+被占用 当前位置向后找空闲位置、存放哈希表（要防止越界）
+
+查询： 通过哈希函数计算数据存储分位置 取值 判断状态STATE_USING
+	该值==查询值 找到了
+		！=   发生哈希冲突 往后遍历寻找该元素
+		1.该位置一直为空 ，没放过元素  ---不需要继续往后搜索
+		2.位置为空 以前放过元素，后来被删除 ---需要往后搜索
+
+删除： 通过哈希函数计算数据存储位置，从该位置取该值，判断状态STATE_USING
+		== 直接求改当前位置的状态就可以 STATE_DEL
+		！= 要删除的值（之前有哈希冲突），从当前位置向后遍历，找到该元素，修改状态，
+		入宫遇到STATE_UNUSE结束
+*/
 #include<iostream>
 #include<algorithm>
 #include<cstring>
@@ -23,25 +38,7 @@ struct Bucket {
 	Bucket(int k = 0, State s = UNUSE) :key(k), state(s) {}
 };
 
-
-/*
-增加： 通过哈希函数 计算存放位置
-该位置空闲 直接存储
-被占用 当前位置向后找空闲位置、存放哈希表（要防止越界）
-
-查询： 通过哈希函数计算数据存储分位置 取值 判断状态STATE_USING
-	该值==查询值 找到了
-		！=   发生哈希冲突 往后遍历寻找该元素
-		1.该位置一直为空 ，没放过元素  ---不需要继续往后搜索
-		2.位置为空 以前放过元素，后来被删除 ---需要往后搜索
-
-删除： 通过哈希函数计算数据存储位置，从该位置取该值，判断状态STATE_USING
-		== 直接求改当前位置的状态就可以 STATE_DEL
-		！= 要删除的值（之前有哈希冲突），从当前位置向后遍历，找到该元素，修改状态，
-		入宫遇到STATE_UNUSE结束
-*/
-
-class HashTable {
+class LinearHashTable {
 private:
 	Bucket* table;//哈希表
 	int tableSize;//总大小
@@ -57,8 +54,8 @@ private:
 		}
 		int newSize = primes[primeIndex + 1];
 		Bucket* newTable = new Bucket[newSize];//新表
-		
-		for (int i = 0; i < primeIndex; i++) {
+		//遍历旧表
+		for (int i = 0; i < tableSize; i++) {
 			if (table[i].state == USING) {
 				int index = table[i].key % newSize;
 				int nextI = index;
@@ -80,7 +77,7 @@ private:
 		table = newTable;
 	}
 public:
-	HashTable(int size = primes[0], int loadFactor = 0.75) :loadFactor(loadFactor),useBucketNum(0), primeIndex(0) {
+	LinearHashTable(int size = primes[0], int loadFactor = 0.75) :loadFactor(loadFactor),useBucketNum(0), primeIndex(0) {
 		// 根据size找到合适的初始大小
 		if (size != primes[0]) {
 			while (primeIndex < PRIME_SIZE - 1 && primes[primeIndex] < size) {
@@ -92,51 +89,60 @@ public:
 	}
 
 	bool insert(int key) {
-		double factor = useBucketNum * 1.0 / tableSize;
-		if (factor > loadFactor) {//扩容
-			expand();
-		}
-		int index = key % tableSize;
-		int i = index;//直接映射到的下标 如果遇到 哈希冲突就往后找
-
-		//哈希冲突 往后找第一个能插入的位置 如果转了一圈都没有找到 那就是return false 了 无法插入？
-		//哈希冲突 去找后续的位置
-		if (table[i].state == USING) {
-			//只要 不越界  +  这个位置不能放 也就是 是USING
-			while (i != index&& table[i].state == USING) {
-				i = (i + 1) % tableSize;
+		if (!find(key)) {
+			double factor = useBucketNum * 1.0 / tableSize;
+			if (factor > loadFactor) {//扩容
+				expand();
 			}
-		}//出循环要么找到位置 能放下   回到index要么无法放下
-		if (i == index) {
-			return false;
-		}
+			int index = key % tableSize;
+			int i = index;//直接映射到的下标 如果遇到 哈希冲突就往后找
 
-		table[i].state = USING;
-		table[i].key = key;
-		useBucketNum++;
-		return true;
+			//哈希冲突 往后找第一个能插入的位置 如果转了一圈都没有找到 那就是return false 了 无法插入？
+			//哈希冲突 去找后续的位置
+			while (table[i].state == USING) {
+				//只要 不越界  +  这个位置不能放 也就是 是USING
+				i = (i + 1) % tableSize;
+				if (i == index) {// 回到 index没有位置
+					return false;
+				}
+			}
+			//出循环找到位置 能放下
+			table[i].state = USING;
+			table[i].key = key;
+			useBucketNum++;
+			return true;
+		}
 	}
 
 	//return true 找到了 并且删除
 	// return false 没找到
-	bool earse(int key) {
+	bool erase(int key) {
 		int index = key % tableSize;
 		int i = index;
 		if (table[i].state == USING && table[i].key == key) {
-			table[i].state = UNUSE;
+			table[i].state = DEL;
+			useBucketNum--;//减少
 			return true;
+		}
+		else if (table[i].state == UNUSE) {
+			//这个位置根本就没有被用过 
+			return false;
 		}
 		//i位置不是 key 那就是往后找一圈
 		i = (i + 1) % tableSize;
-		while (i != index && !(table[i].key != key && table[i].state == USING)) {
+		while (i != index) {
+			//找到了就删除
+			if (table[i].key == key && table[i].state == USING) {
+				table[i].state = DEL;
+				useBucketNum--;
+				return true;
+			}
+			//优化 如果遇到了UNUSE 说明后续肯定不可能存在了
+			if (table[i].state == UNUSE) {
+				return false;
+			}
 			i = (i + 1) % tableSize;
 		}
-		//这个位置就是存储的位置
-		if (table[i].key == key && table[i].state == USING) {
-			table[i].state = DEL;
-			return true;
-		}
-		// 还是没找到
 		return false;//不存在
 	}
 
@@ -144,7 +150,10 @@ public:
 	bool find(int key) {
 		int index = key % tableSize;
 		int i = index;
-
+		//如果这个桶没用过 --->false
+		if (table[i].state == UNUSE) {
+			return false;
+		}
 		if (table[i].state==USING&&table[i].key == key) {
 			return true;
 		}
@@ -152,6 +161,9 @@ public:
 		while (i != index) {
 			if (table[i].state == USING && table[i].key == key) {
 				return true;
+			}
+			if (table[i].state == UNUSE) {
+				return false;//如果遇到UNUSE就是没有
 			}
 			i = (i + 1) % tableSize;
 		}
--- Problems/linkedListHashTable.cpp
+++ Problems/linkedListHashTable.cpp
@@ -1,1 +1,141 @@
 ﻿
+// 链式哈希表
+/*
+线性探测哈希表的缺陷：
+1.发生哈希冲突时，靠近O(n)的时间复杂度，存储变慢了   在线性探测哈希表中无法优化
+
+2.多线程环境中，线性探测表所用到的基于数组实现的哈希表
+只能给全局的哈希表用互斥锁来保证哈希表的原子操作，保证线程安全
+//多个线程中无法在一个数组中操作   互斥操作 保证安全
+
+当两个或多个线程并发执行时，若都发生哈希冲突 向后寻找空闲位置时 最终后放数据的线程将前者的数据覆盖、
+
+
+无序关联容器 采用链式哈希表
+
+链式哈希表：
+多线程时，使用分段的锁既保证线程的安全又有一定大并发量，提高了效率
+
+12 18 21 24 33 45 67 72   除留余数法 +链地址法
+
+tableSize_=7
+链结点
+vector<list<int>>table;
+在每一个哈希桶中利用一个分段锁来控制该桶
+0   |21| |
+1
+2	|72| |
+3	|24| | ——|45| |
+4   |18| | ——|67| |
+5   |12| | ——|33| |
+6
+
+增加：O(1)
+搜索 21
+21%7=0 ；O（1）
+此时为 table[0] O(1)
+删除 遍历链表 O（n)
+
+解决数据过多，链表过长 效率低
+优化1：当链表长度大于8（10），把桶里面的这个链表转化成红黑树O（log n）
+//但红黑树每个节点花费内存（存储地址多）比链表多
+
+优化2：链式哈希表每个桶都可以创建自己的互斥锁，不同桶中的链表操作，可以并发执行起来；
+散列结果越散越好 ——最少冲突
+
+哈希表：无限趋向于O（1） 哈希冲突的存在
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<list>
+#include<functional>
+#include<algorithm>
+using namespace std;
+
+const int PRIME_SIZE = 9;
+int primes[PRIME_SIZE] = { 3,7,23,47,97,251,443,1471,42773 };
+
+class linkedListHashTable {
+public:
+	linkedListHashTable(int tableSize = primes[0], double lF = 0.75) :tableSize(tableSize),usedBucketNums(0), primeIndex(0),loadFactor(lF) {
+		int i = 0;
+		if (tableSize > primes[0]) {//需要去找合适的大小
+			while (i < PRIME_SIZE - 1 && tableSize > primes[i]) {
+				i++;
+			}//出循环 tableSize<=primes[i] || i ==PRIME_SIZE
+		}
+		primeIndex = i;
+		tableSize = primes[i];
+		table.resize(primes[i]);
+	}
+
+	//不重复加入元素
+	void Insert(int key) {
+		double lF =  usedBucketNums * 1.0 / tableSize;
+		if (lF > loadFactor) {
+			expand();
+		}
+		int index = key % tableSize;
+		//直接加入 or 存在元素 check是否存在
+		if (table[index].empty()) {
+			table[index].push_front(key);
+			usedBucketNums++;
+		}
+		else {
+			//链表遍历 查找key
+			auto it = ::find(table[index].begin(), table[index].end(), key);
+			if (it == table[index].end()) {
+				table[index].push_front(key);
+			}
+		}
+	}
+
+	void erase(int key) {
+		int index = key % tableSize;
+		auto it = ::find(table[index].begin(), table[index].end(), key);
+		if (it != table[index].end()) {
+			table[index].erase(it);
+		}
+		if (table[index].empty()) {
+			usedBucketNums--;
+		}
+	}
+
+	bool find(int key) {
+		int index = key % tableSize;
+		auto it = ::find(table[index].begin(), table[index].end(), key);
+		return it != table[index].end();
+	}
+
+	void expand() {
+		primeIndex += primeIndex == PRIME_SIZE - 1 ? 0 : 1;
+		tableSize = primes[primeIndex];
+		usedBucketNums = 0;
+		vector<list<int>>oldTable;
+		table.swap(oldTable);//在栈上的三个指针 交换即可 效率高
+		table.resize(primes[primeIndex]);
+
+		for (auto& list : oldTable) {
+			for (auto& key : list) {
+				int index = key % tableSize;
+				if (table[index].empty()) {
+					usedBucketNums++;
+				}
+				//无需考虑去重了 原本就是去重之后的
+				table[index].push_front(key);
+			}
+		}
+
+	}
+
+private:
+	vector<list<int>>table;
+	int tableSize;
+	int usedBucketNums;// 桶的个数
+	double loadFactor;
+	int primeIndex;
+};
+
+
