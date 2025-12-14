3ff4440050a1dd350ce8c6f517f1363cf576fd04
aliao <aliao2414937928@qq.com>
2025-12-14 19:48:34 +0800

    modify code

--- LinkedList/CircleLinkedList.cpp
+++ LinkedList/CircleLinkedList.cpp
@@ -7,44 +7,143 @@
 using namespace std;
 
 struct Node {
-	int val;
+	int data;
 	Node* next;
-	Node(int v = -1, Node* n = nullptr):val(v),next(n){}
+	Node(int v = -1, Node* n = nullptr):data(v),next(n){}
 };
+
+
+// 约瑟夫环问题 - 不带头结点的单项循环链表应用
+void Joseph(Node* head, int k, int m)
+{
+	Node* p = head;
+	Node* q = head;
+
+	// q指向最后一个
+	while (q->next != head)
+	{
+		q = q->next;
+	}
+
+	// 从第k个人开始报数的
+	for (int i = 1; i < k; i++)
+	{
+		q = p;
+		p = p->next;
+	}
+
+	// p -> 第k个人
+	for (;;)
+	{
+		for (int i = 1; i < m; i++)
+		{
+			q = p;
+			p = p->next;
+		}
+
+		// 删除p指向的结点
+		// q p node
+		cout << p->data << " ";
+
+		if (p == q)
+		{
+			delete p;
+			break;
+		}
+
+		q->next = p->next;
+		delete p;
+		p = q->next;
+	}
+}
+
 
 class circleLinkedList {
 private:
 	//循环链表实现
-	Node* front;
-	Node* rear;
+	Node* head;
+	Node* tail;
 public:
-	circleLinkedList() :front(nullptr), rear(nullptr) {
+	circleLinkedList() {
+		head = new Node();
+		tail = head;
+		head->next = head;
+		//一个头节点 自己指向自己
+	}
 
+	~circleLinkedList() {
+		//整个类析构
+		Node* cur = head->next;
+		while (cur != nullptr) {
+			head->next = cur->next;
+			delete cur;
+			cur = head->next;
+		}
+		delete head;
 	}
 
 	bool isEmpty() {
-		return front == rear;
-	}
-
-	bool isFull() {//
-		//为 nullptr ---> front == rear 啥也没有
-		// 否则就是...
-		return (rear == nullptr) || (rear->next == front);
+		//都指向head节点
+		return head == tail;
 	}
 
 	void insertHead(int val) {
-
+		Node* newNode = new Node(val);
+		newNode->next = head->next;
+		head->next = newNode;
+		// 初始化的时候 只存在一个头节点 head  
+		// 做完上述之后 newNode->next = head --->需要移动尾指针
+		if (newNode->next == head) {
+			tail = newNode;
+		}
 	}
 
 	void insertTail(int val) {
+		Node* newNode = new Node(val);
+		newNode->next = tail->next;
+		// 倒数第二个节点指向倒数第一个节点
+		tail->next = newNode;
+		tail = newNode;
 
 	}
 
-	void removeHead(int val) {
-
+	void Remove(int val) {
+		Node* pre = head;
+		Node* cur = head->next;
+		if (cur == nullptr) {
+			return;
+		}
+		while (cur != head) {
+			if (cur->data == val) {
+				//删除 cur节点
+				pre->next = cur->next;
+				delete cur;
+				//如果 删除的是尾节点 把tail设置为pre
+				if (pre->next == head) {
+					tail = pre;
+				}
+				return;
+			}
+			pre = cur;
+			cur = cur->next;
+		}
 	}
 
-	void removeTail(int val) {
-
+	bool find(int val)const {
+		Node* cur = head->next;//带头结点查询
+		//没有回到head
+		while (cur != head) {
+			if (cur->data==val) {
+				return true;
+			}
+		}
+		return false;
+	}
+	void show() {
+		Node* cur = head->next;
+		while (cur != head) {
+			cout << cur->data << " ";
+			cur = cur->next;
+		}
 	}
 };
--- LinkedList/DoubleLinkedList.cpp
+++ LinkedList/DoubleLinkedList.cpp
@@ -1,1 +1,75 @@
 ﻿
+#include<iostream>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* pre;
+	Node* next;
+	Node(int data=-1,Node*pre=nullptr,Node*next):data(data),pre(pre),next(next){}
+};
+
+class doubleLinkedList {
+private:
+	Node* head;//头节点
+public:
+	doubleLinkedList() {
+		head = new Node();
+	}
+
+	~doubleLinkedList() {
+		Node* cur = head;
+		while (cur != nullptr) {
+			head = head->next;
+			delete cur;
+			cur = head->next;
+		}
+	}
+
+	void insertHead(int val) {
+		Node* newNode = new Node(val);
+		newNode->next = head->next;
+		newNode->pre = head;
+		if (newNode->next != nullptr){
+			newNode->next->pre = newNode;
+		}
+		head->next = newNode;
+	}
+	
+	void insertTail(int val) {
+		Node* newNode = new Node(val);
+		Node* cur = head;
+		while(cur->next!=nullptr){
+			cur = cur->next;
+		}
+		cur->next = newNode;
+		newNode->pre = cur;
+	}
+	
+	void Remove(int val) {
+		Node* cur = head->next;
+		while (cur != nullptr) {
+			if (cur->data == val) {
+				// 前一个节点
+				Node* pre = cur->pre;
+				pre->next = cur->next;
+				if (cur->next != nullptr) {
+					cur->next->pre = pre;
+				}
+				delete cur;
+				return;
+			}
+			cur = cur->next;
+		}
+	}
+	bool find(int val) {
+		Node* cur = head->next;
+		while (cur != nullptr) {
+			if (cur->data == val) {
+				return true;
+			}
+			cur = cur->next;
+		}
+		return false;
+	}
+};
